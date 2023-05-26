53190b291ca67f1b664d9891978640d4d233c3fe
Rasul-code <sadlarkhanov-rasul@mail.ru>
2023-05-26 21:08:29 +0400

    added custom  model for user

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
+class User(AbstractUser):
+    email = models.EmailField(
+        'Почта',
+        max_length=254,
+        unique=True
+    )
+    first_name = models.CharField(
+        'Имя',
+        max_length=150
+    )
+    last_name = models.CharField(
+        'Фамилия',
+        max_length=150
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.email
